d0d53441f049256b683b229e4b42977f2687b834
Andrew Brady <brady8424@outlook.com>
2022-12-07 05:17:57 -0600

    Testing Modifications from test project

--- Android/PalmTouchDemo/app/src/main/java/io/interactionlab/palmtouchdemo/FullscreenActivity.java
+++ Android/PalmTouchDemo/app/src/main/java/io/interactionlab/palmtouchdemo/FullscreenActivity.java
@@ -4,8 +4,16 @@ package io.interactionlab.palmtouchdemo;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+
+
+
+//import android.support.v7.app.ActionBar;
+import androidx.appcompat.app.ActionBar;
+
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 import android.view.View;
 import android.widget.RelativeLayout;
 
